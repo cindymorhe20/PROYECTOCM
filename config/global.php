6df430ca4af7153a4ac672bf71f22d872f85a1dc
cindymorhe20 <cmoralesh5@miumg.edu.gt>
2020-04-14 14:21:37 -0600

    carpetas y conexion de DBejemplo

---
+++ config/global.php
@@ -0,0 +1,37 @@
+
+<?php
+
+//ip del servidor xampp
+
+//es importante definir la conexion a la db. eje. 192.168.1.100:81
+
+define("DB_HOST", "localhost");
+
+
+//nombre de la base datos
+
+define("DB_NAME", "dbsistemas");
+
+
+//usuario de la base de datos
+
+define("DB_USERNAME", "root"); //aca se recomienda cambiarlo por uno propio.
+
+
+//password del usuario
+
+define("DB_PASSWORD", "");
+
+
+//definir la codificacion de caracteres
+
+define("DB_ECONDE", "utf8");
+
+
+//definir una constante con el nombre
+
+define("PRO_NOMBRE", "SISTEM 2020");
+
+
+
+ ?>
